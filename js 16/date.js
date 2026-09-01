ae8d604220c44e215e1c66ea62fa2a8dfbdaf43c
Farzan khan <farzaan341@gmail.com>
2026-09-02 00:20:13 +0500

    Add files via upload

---
+++ js 16/date.js
@@ -0,0 +1,31 @@
+let today = new Date();
+console.log(today);
+
+
+let activeYear = new Date;
+console.log(activeYear.getFullYear());
+
+
+let activeMonth = new Date();
+console.log(activeMonth.getMonth());
+
+
+let currentTime = new Date();
+
+console.log(currentTime.getHours());
+console.log(currentTime.getMinutes());
+console.log(currentTime.getSeconds());
+
+let  currentYear = new Date();
+console.log(today.getFullYear());
+currentYear.setFullYear(2030);
+console.log(currentYear.getFullYear());
+
+
+let date = new Date();
+
+date.setFullYear(2027);
+date.setMonth(11);
+date.setDate(28);
+
+console.log(date);
